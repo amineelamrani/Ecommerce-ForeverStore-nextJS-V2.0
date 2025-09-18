6fa4025956934818e6977a8c4373f58da2ca55f2
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-09-18 18:57:43 +0100

    Adding toggles for size selection

---
+++ src/components/productPageComponents/SizesSelectionSection.tsx
@@ -0,0 +1,28 @@
+"use client";
+import React, { useState } from "react";
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+
+export default function SizesSelectionSection({ sizes }: { sizes: string[] }) {
+  const [size, setSize] = useState("");
+  return (
+    <div className="flex flex-col">
+      <h4>Select Size</h4>
+      <ToggleGroup
+        type="single"
+        className="gap-2 rounded-none"
+        onValueChange={(e) => setSize(e)}
+      >
+        {sizes.map((size, index) => (
+          <ToggleGroupItem
+            value={size}
+            className="bg-slate-200 rounded-sm py-2 hover:cursor-pointer"
+            aria-label="Toggle bold"
+            key={index}
+          >
+            {size}
+          </ToggleGroupItem>
+        ))}
+      </ToggleGroup>
+    </div>
+  );
+}
