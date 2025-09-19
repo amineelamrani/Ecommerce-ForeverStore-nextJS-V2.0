2e948aeb945c879a2ef0fccedbccf3e2b180bb40
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-09-19 11:28:00 +0100

    completing the review tab section

--- src/components/productPageComponents/ReviewTabSectionItems.tsx
+++ src/components/productPageComponents/ReviewTabSectionItems.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { ReviewInterface } from "@/models/review";
+import { ReviewInterface, userDoc } from "@/models/review";
 import { fetchProductReviews } from "@/serverActions/actions";
 import React, { useEffect, useState } from "react";
+import FiveStarsFeedback from "../FiveStarsFeedback";
+import { ReviewsSkeleton } from "../ReviewsSkeleton";
 
 interface reviewNeededItems {
   ownerName: string;
@@ -31,7 +33,7 @@ export default function ReviewTabSectionItems({
       if (getReviews.status === "success") {
         const newReviewsArray = JSON.parse(getReviews.result!).map(
           (review: ReviewInterface) => ({
-            ownerName: review.owner.name,
+            ownerName: (review.owner as userDoc).name,
             content: review.content,
             rating: review.rating,
             createdAt: review.createdAt,
@@ -49,5 +51,37 @@ export default function ReviewTabSectionItems({
 
   // Now fetchedReviews are ready to be integrated in the reviews tab section + Need to have loading effect when fetching or when opening the tab first time
 
-  return <div>ReviewTabSectionItems</div>;
+  return (
+    <>
+      {fetchedReviews &&
+        fetchedReviews.length > 0 &&
+        fetchedReviews.map((review) => (
+          <div
+            className="w-full px-3 py-2 bg-slate-100 rounded-lg"
+            key={`review-${review.createdAt}`}
+          >
+            <div className="flex flex-row justify-between items-center pb-2">
+              <div className="flex flex-col">
+                <h1 className="font-bold text-xs sm:text-sm md:text-base">
+                  {review.ownerName}
+                </h1>
+                <FiveStarsFeedback size={10} rating={review.rating} />
+              </div>
+              <h3 className="text-xs sm:text-sm md:text-base">
+                {review.createdAt.split("T")[0]}
+              </h3>
+            </div>
+            <h2 className="font-bold text-xs md:text-base">{title}</h2>
+            <p className="text-xs sm:text-sm md:text-base text-gray-500">
+              {review.content}
+            </p>
+          </div>
+        ))}
+      {!fetchedReviews && (
+        <div className="w-full px-3 py-2 rounded-lg">
+          <ReviewsSkeleton />
+        </div>
+      )}
+    </>
+  );
 }
