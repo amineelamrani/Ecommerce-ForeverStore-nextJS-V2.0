6f00dc5ff28f3aebc31e09ffcae4a907d88758c8
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-09-25 18:34:12 +0100

    adding stripe payment in the server

--- src/serverActions/stripeActions.ts
+++ src/serverActions/stripeActions.ts
@@ -21,7 +21,33 @@ export const orderServerAction = async (
   initialState: InitialOrderingInterface,
   formData: FormData
 ) => {
+  console.log("Server Action invoqued");
+  if (
+    !formData.get("productsToBuy") ||
+    !formData.get("paymentMethod") ||
+    !formData.get("firstName") ||
+    !formData.get("lastName") ||
+    !formData.get("email") ||
+    !formData.get("street") ||
+    !formData.get("city") ||
+    !formData.get("state") ||
+    !formData.get("country") ||
+    !formData.get("zipCode") ||
+    !formData.get("phone")
+  ) {
+    return {
+      redirectingUrl: "",
+      message: "",
+      paymentMethod: "card",
+      error: {
+        error: true,
+        message: "Please Provide all The fields!",
+      },
+    };
+  }
+
   const data = Object.fromEntries(formData);
+
   const {
     productsToBuy,
     paymentMethod,
@@ -35,6 +61,7 @@ export const orderServerAction = async (
     zipCode,
     phone,
   } = data;
+
   if (
     productsToBuy !== "" &&
     paymentMethod !== "" &&
@@ -94,6 +121,63 @@ export const orderServerAction = async (
         success_url: `${process.env.REDIRECTING_URL}?success=true&session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.REDIRECTING_URL}?canceled=true`,
       });
+      // Start
+
+      let subTotal = 0,
+        total = 0;
+      for (let i = 0; i < jsonProductToBuy.length; i++) {
+        subTotal += jsonProductToBuy[i].quantity * jsonProductToBuy[i].price;
+      }
+      total = subTotal + 10;
+
+      const products = jsonProductToBuy.map((item: BasketContentType) => {
+        return {
+          productID: item.id,
+          size: item.size,
+          quantity: item.quantity,
+          title: item.title,
+        };
+      });
+      await dbConnect();
+      const userId = await getUserId();
+
+      const newOrder = await Order.create({
+        products,
+        owner: userId,
+        deliveryInformation: {
+          firstName: firstName.toString(),
+          lastName: lastName.toString(),
+          email: email.toString(),
+          street: street.toString(),
+          city: city.toString(),
+          state: state.toString(),
+          country: country.toString(),
+          zipCode: Number(zipCode),
+          phone: Number(phone),
+        },
+        payment: {
+          method: "cod",
+          status: "pending",
+          money: total,
+        },
+        statusDelivery: "Order Placed",
+      });
+
+      const actualUser = await User.findById(userId);
+      actualUser.orders.push(newOrder._id);
+      actualUser.confirmPassword = actualUser.password;
+      await actualUser.save();
+
+      const arrayProducts = jsonProductToBuy.map(
+        async (item: BasketContentType) => {
+          const productImpacted = await Product.findById(item.id);
+          productImpacted.ordersNumber++;
+          await productImpacted.save();
+          return productImpacted;
+        }
+      );
+
+      // End
 
       return {
         redirectingUrl: session.url,
@@ -194,6 +278,16 @@ export const orderServerAction = async (
           },
         };
       }
+    } else {
+      return {
+        redirectingUrl: "",
+        message: "",
+        paymentMethod: "card",
+        error: {
+          error: true,
+          message: "Not Supported Payment Mode!",
+        },
+      };
     }
   } else {
     return {
@@ -204,6 +298,44 @@ export const orderServerAction = async (
         error: true,
         message: "Invalid Delivery Information, Please provide Correct infos!",
       },
+    };
+  }
+};
+
+export const checkStripeSuccess = async (session_id: string) => {
+  // This server function to check if the stripe payment is done successfully  or the user try to trick us
+  // Url returned when success : '${process.env.REDIRECTING_URL}?success=true&session_id={CHECKOUT_SESSION_ID}'
+  // So I will received the session_id from the client, and I will check if it is done successfully or not then will return a feedback to the browser if it is succeded or not
+  if (!session_id) {
+    return {
+      status: "fail",
+      message:
+        "Please provide the session id, to check if the payment done successfully",
+    };
+  }
+
+  try {
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+    console.log(session);
+    if (session.payment_status === "paid") {
+      // Then at this moment we need to update the database to take that command into consideration
+      // But here if we decided to add it to the database in this function then we will call the server with the same data two time
+      // /!\ Decision -> when we call this form function "orderServerAction" we will create an order but with unpaid status and after the stripe payment, if success here then we pass the status to paid else we delete that Order from the database
+      return {
+        status: "success",
+        message: "Payment Done successfully",
+      };
+    } else {
+      return {
+        status: "fail",
+        message: "Not Paid",
+      };
+    }
+  } catch (err) {
+    return {
+      status: "fail",
+      message:
+        "Internal Error while trying to proceed with Payment, Please Try Again",
     };
   }
 };
