4208d9d1011652ff51baa8f3aa1069540ead0532
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-09-26 19:26:45 +0100

    adding server functions for isPurchased and isReviewed

--- src/serverActions/actions.ts
+++ src/serverActions/actions.ts
@@ -1,7 +1,8 @@
 "use server";
 
-import { Product, Review } from "@/models/models";
+import { Product, Review, User } from "@/models/models";
 import { revalidateTag } from "next/cache";
+import { getUserId } from "./authActions";
 
 export const fetchProductReviews = async (productID: string | null) => {
   if (!productID) {
@@ -47,6 +48,48 @@ export const fetchRelatedProduct = async (
     status: "success",
     result: JSON.stringify(relatedProducts),
   };
+};
+
+export const isProductPurchased = async (productID: string) => {
+  // This server function to get all the products purchased by the currentUser
+  const userId = await getUserId();
+  if (!userId) {
+    return false;
+  }
+  const impactedUser = await User.findById(userId)
+    .select("orders")
+    .populate("orders", "products statusDelivery");
+
+  // We have array of impactedUser.orders and inside each array a nest array of .products
+
+  for (let i = 0; i < impactedUser.orders.length; i++) {
+    const { statusDelivery } = impactedUser.orders[i];
+    if (statusDelivery === "Delivered") {
+      for (let j = 0; j < impactedUser.orders[i].products.length; j++) {
+        if (impactedUser.orders[i].products[j].productID === productID) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+};
+
+export const isProductReviewed = async (productID: string) => {
+  // This given the currentUser and the productID will check whether the user has already reviewed that product
+  const userId = await getUserId();
+  if (!userId) {
+    return false;
+  }
+  const userReviews = await Review.find({
+    product: productID,
+    owner: userId,
+  });
+  if (userReviews && userReviews.length > 0) {
+    return true;
+  }
+  return false;
 };
 
 export const revalidateTagTest = async () => {
