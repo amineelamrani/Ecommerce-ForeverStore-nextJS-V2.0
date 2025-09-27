ce7313c0fefc76516515b1a45aff1f5b4e519454
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-09-28 00:02:27 +0100

    Fixing middleware : using jose insead of jsonwebtoken (recommended for edge runtime)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt, { JwtPayload } from "jsonwebtoken";
+// import jwt, { JwtPayload } from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
-export function middleware(request: NextRequest) {
-  console.log("Hi middleware");
+export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   if (pathname.startsWith("/purchased")) {
     const cookie = request.cookies.get("foreverEcommNext_2.0");
@@ -12,16 +12,14 @@ export function middleware(request: NextRequest) {
     if (!token) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
-    console.log("token : ", token);
     try {
-      const decoded = jwt.verify(token, process.env.SECRET_JWT_KEY!);
-      console.log("decoded : ", decoded);
-      if (typeof decoded === "string") {
+      const secret = new TextEncoder().encode(process.env.SECRET_JWT_KEY);
+      const { payload } = await jwtVerify(token, secret);
+
+      if (typeof payload === "string") {
         return NextResponse.redirect(new URL("/login", request.url));
       }
-      const id = (decoded as JwtPayload).id;
-      console.log(id);
-      if (id && id !== "" && id.length > 1) {
+      if (payload.id) {
         return NextResponse.next();
       }
       return NextResponse.redirect(new URL("/login", request.url));
